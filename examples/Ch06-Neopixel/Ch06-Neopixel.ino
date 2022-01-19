74d9c3d9db75713d3d2a8772acc93fcf9fe1023d
HigherOrderInnovation <70900928+HigherOrderInnovation@users.noreply.github.com>
2022-01-18 21:04:56 -0600

    Update Ch06-Neopixel.ino
    LED number updated

--- examples/Ch06-Neopixel/Ch06-Neopixel.ino
+++ examples/Ch06-Neopixel/Ch06-Neopixel.ino
@@ -12,29 +12,32 @@ IRIS robot;         // Create an Iris object called robot
 // Additional set-up required for Neopixel
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN    robot.getNeoPin()
-#define LED_COUNT 2
+#define LED_COUNT 3
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {    // Initialize NeoPixel strip object
   strip.begin();           
-  strip.show();            
+  strip.show();   
+  strip.setBrightness(100);  
 }
 
-uint32_t color = strip.Color(255,0,0);  // Choose the color here.
+uint32_t color = strip.Color(0,0,0);  // Choose the color here.
 
 void loop() {
-  for (int i = 0; i <= 50; i = i+1){     // The variable i will INCREASE from 0 to 50 by an increment of 1
-    strip.setBrightness(i);             
+  for (int i = 0; i <= 255; i = i+5){     // The variable i will INCREASE from 0 to 50 by an increment of 1
+    color = strip.Color(i,0,0);          
     strip.setPixelColor(0, color);      // Use the color variable
     strip.setPixelColor(1, color);      // Use the color variable
+    strip.setPixelColor(2, color);      // Use the color variable
     strip.show();                       // Display the new colors
     delay(50);                          // Wait 50 milliseconds
   }
 // Reverse the fading sequence:
-  for (int i = 50; i >= 0; i = i-1){     // The variable i will DECREASE from 50 to 0 by an increment of 1
-    strip.setBrightness(i);             
+  for (int i = 255; i >= 0; i = i-5){     // The variable i will DECREASE from 50 to 0 by an increment of 1
+    color = strip.Color(i,0,0);             
     strip.setPixelColor(0, color);      // Use the color variable
     strip.setPixelColor(1, color);      // Use the color variable
+    strip.setPixelColor(2, color);      // Use the color variable
     strip.show();                       // Display the new colors
     delay(50);                          // Wait 50 milliseconds
   }
